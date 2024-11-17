ab3760f604ee6d045e2699fb852664794391594d
Alex Kubiaki <51460277+ALEXKUBIAKI@users.noreply.github.com>
2024-11-17 14:48:18 -0300

    Update server.js

--- server.js
+++ server.js
@@ -2,10 +2,12 @@
 const express = require('express');
 const studentRoutes = require('./routes/studentRoutes');
 const errorHandler = require('./middlewares/errorHandler');
+const cors = require('cors');
 
 const app = express();
 const PORT = 3000;
 
+app.use(cors());
 app.use(express.json());
 app.use('/students', studentRoutes);
 app.use(errorHandler); // Middleware de tratamento de erros
